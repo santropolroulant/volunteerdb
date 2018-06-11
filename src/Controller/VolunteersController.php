93e730bec439a15d9b2a8a6b72de21395d648c40
Nick Guenther <kousu@santropolroulant.org>
2018-06-11 12:02:27 -0400

    DRY up /edit code as much as possible.
    There's several requests that pass through this function now,
    but they're all pretty similar so I've unified them.

--- src/Controller/VolunteersController.php
+++ src/Controller/VolunteersController.php
@@ -21,6 +21,8 @@ class VolunteersController extends AppController {
         #TODO make this "upcoming birthdays"
         #search for birthmonth = lastmonth, thismonth, nextmonth
         #remember about dec/jan wrap!
+        # TODO: replace this 'birthmonth in' thing with using datetime <=
+        #       see https://github.com/santropolroulant/volunteerdb/issues/5
         $thisMonth = date("n");
         $nextMonth = $thisMonth + 1 == 13 ? 1 : $thisMonth + 1;
         $lastMonth = $thisMonth - 1 == 0 ? 12 : $thisMonth - 1;
@@ -91,56 +93,82 @@ class VolunteersController extends AppController {
 
     public function edit($id = null) {
 
-        $volunteer = $this->Volunteers->query()->where(["id" => $id])->first(); # look up the volunteer here, to share code paths between GET/POST (if we're in the POST->new subpath, this will just be null and that's okay)
-
-        if ($this->request->is('post') || $this->request->is('put')) {
-            # XXX for some reason CakePHP seems to rewrite POST requests for *edits* to 'PUT' requests
-            # but a POST request for a new entity is still POST. what?
-
-            $data = $this->request->getData();
-
-            # glue the DatePicker widget into our silly three-column birthdate setup
-            # TODO: this will be dropped once we do https://github.com/santropolroulant/volunteerdb/issues/5
-            if($data["birthdate"]) {
-              $_birthdate = new DateTime($data["birthdate"]);
-              $data["birthday"] = $_birthdate->format('d');
-              $data["birthmonth"] = $_birthdate->format('m');
-              $data["birthyear"] = $_birthdate->format('Y');
-            } else {
-              $data["birthday"] = "";
-              $data["birthmonth"] = "";
-              $data["birthyear"] = "";
-            }
+        $data = $this->request->getData();
+        # Unpack the birthdate field into the older birthday/month/year fields
+        # TODO: this will be dropped once we do https://github.com/santropolroulant/volunteerdb/issues/5
+        if(isset($data["birthdate"])) {
+            $_birthdate = new DateTime($data["birthdate"]);
+            $data["birthday"] = $_birthdate->format('d');
+            $data["birthmonth"] = $_birthdate->format('m');
+            $data["birthyear"] = $_birthdate->format('Y');
             unset($data["birthdate"]);
+        } else {
+            $data["birthday"] = "";
+            $data["birthmonth"] = "";
+            $data["birthyear"] = "";
+        }
 
-            if($volunteer) {
-                # it's an edit
-                $this->Volunteers->patchEntity($volunteer, $data);
+
+        # Determine the active Volunteer object
+        if($this->request->is(['get', 'put'])) {
+            # read/update loads the current state from the DB
+            if($id) {
+                $volunteer = $this->Volunteers
+                                  ->query()
+                                  ->where(["id" => $id])
+                                  ->first();
+
+                # /edit/{id} is only okay if $id references a real volunteer
+                if(!$volunteer) {
+                    throw new NotFoundException();
+                }
             } else {
-                # it's a create
-                $volunteer = $this->Volunteers->newEntity($data);
+                # /edit/ is always okay: it means create a new volunteer
+                # but we don't actually create the object here yet; we first load an empty form
+                # and then in the POST when the user submits that form back to us,
+                # there's where we call newEntity().
+                $volunteer = NULL;
             }
 
+        } else if ($this->request->is('post')) {
+            # Create
+            $volunteer = $this->Volunteers->newEntity($data);
+        }
+
+        # Update
+        if($this->request->is('put')) {
+            $this->Volunteers->patchEntity($volunteer, $data);
+        }
+
+
+        # Write the volunteer object to the DB, as requested
+        if($this->request->is(['post', 'put'])) {
+            # Create/Update saves the changes back
             if ($this->Volunteers->save($volunteer)) {
                 $this->Flash->success('Data saved.');
                 return $this->redirect(array('action' => 'view', $volunteer["id"]));
             } else {
                 $this->Flash->error('Unable to save data.');
             }
-        }
-        else if ($this->request->is('delete')) {
+        } else if ($this->request->is('delete')) {
             # XXX TODO there should be error-checking wrapped around this execute() call
-            if($this->Volunteers->query()->delete()->where(["id" => $volunteer["id"]])->execute()) {
+            if($this->Volunteers->query()->delete()->where(["id" => $id])->execute()) {
                 $this->Flash->success('Deleted.');
                 return $this->redirect("/"); # this is important; otherwise it will try to load the non-existent delete.ctp file
             } else {
                 $this->Flash->error('Deletion failed.'); # TODO: extract better error message?
+                # this should bounce us back to the same /edit/{id} page we were on when the user clicked Delete
             }
         }
 
-        # again: an awkward temporary glue between two different data storage systems
+        # For get/post/put (i.e. read/create/update), *after* saving to the DB,
+        # shove in birthdate as an addendum field
+        # This is a stepping-stone towards https://github.com/santropolroulant/volunteerdb/issues/5
         try {
           $volunteer["birthdate"] = new DateTime($volunteer["birthyear"]."-".$volunteer["birthmonth"]."-".$volunteer["birthday"]);
+          unset($volunteer["birthday"]);
+          unset($volunteer["birthmonth"]);
+          unset($volunteer["birthyear"]);
         } catch(\Exception $e) {
         }
         
